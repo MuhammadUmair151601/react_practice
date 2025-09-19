cf45c541ebe7e0193909882d501e57805c8e767b
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-09-19 16:34:13 +0500

    pakistan clock designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+// import React from 'react'
+// import Header from './components/Header'
+// import Input from './components/Input'
+// const App = () => {
+//   return (
+//     <div className="h-[470px] w-[550px] flex justify-center ml-100">
+//     <div className="flex flex-col items-center">
+//       <Header></Header>
+//       <Input></Input>
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
-import Header from './components/Header'
-import Input from './components/Input'
+import ClockHeading from './components/ClockHeading'
+import ClockSlogan from './components/ClockSlogan'
+import Time from './components/time'
 const App = () => {
   return (
-    <div className="h-[470px] w-[550px] flex justify-center ml-100">
-    <div className="flex flex-col items-center">
-      <Header></Header>
-      <Input></Input>
-    </div>
-    </div>
+    <center>
+      <ClockHeading></ClockHeading>
+      <ClockSlogan></ClockSlogan>
+      <Time></Time>
+    </center>
   )
 }
 
