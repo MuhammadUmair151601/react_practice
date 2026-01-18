3484b202eff691f4735bc5f05f944908fb99dcc5
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-18 12:45:33 +0500

    files correction of 02 done

--- 02_components_props/src/App.jsx
+++ 02_components_props/src/App.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>This is a sample</h1>
-    </div>
+    <div className='bg-blue-400'>Muhammad umair</div>
   )
 }
 
-export default App;
+export default App
