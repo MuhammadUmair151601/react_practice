dbf86c76880f997ad388ddb35484071ab70826b2
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-21 15:58:37 +0500

    props using json data done

--- 03_cards_proj1/README.md
+++ 03_cards_proj1/README.md
@@ -14,3 +14,60 @@ The React Compiler is not enabled on this template because of its impact on dev 
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+ <!-- {/* <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlLbZlt3X2TpFksku9MgE-cs0oxTzqAH36CQ&s"
+        jobname="Amazon"
+        days="5 days ago"
+        jobTitle="Senior UI/UX designer"
+        location="Texas, USA"
+      /> */}
+      {/* <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZI78WvDPQ241thhVYKtVphlc_J01LbnFVqA&s"
+        jobname="Google"
+        days="2 days ago"
+        jobTitle="Graphic designer"
+        location="Florida, USA"
+      /> */}
+      {/* <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zdLpQ1XogOHnpR9MuW1MD9oXYxuPS5FCgQ&s"
+        jobname="Dribble"
+        days="2 days ago"
+        jobTitle="Senior motion designer"
+        location="London, UK"
+      /> */}
+      {/* <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGGLk0CbrAGDbYcrxy5g2mDeoc4_FM6tNc5w&s"
+        jobname="Figma"
+        days="5 days ago"
+        jobTitle="UX designer"
+        location="Pakistan, Karachi"
+      /> */}
+      {/* <Card
+        image="https://i.pinimg.com/736x/70/80/b2/7080b24c3980c8f4d29412746b5c7ce9.jpg"
+        jobname="Airbnb"
+        days="6 days ago"
+        jobTitle="Junior UX designer"
+        location="Florida, USA"
+      /> */}
+      {/* <Card
+        image="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-history-apple-inc-wikipedia-22.png"
+        jobname="Apple"
+        days="8 days ago"
+        jobTitle="Senior graphic designer"
+        location="Florida, USA"
+      /> */}
+      {/* <Card
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3AjLz2Ze01BXCesLCvOhwrlz9qHboz6XEwQ&s"
+        jobname="Microsoft"
+        days="9 days ago"
+        jobTitle="Data Analyst"
+        location="Japan, Tokyo"
+      /> */}
+      {/* <Card
+        image="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-history-apple-inc-wikipedia-22.png"
+        jobname="Apple"
+        days="8 days ago"
+        jobTitle="Senior graphic designer"
+        location="NYC, USA"
+      /> */} -->
--- 03_cards_proj1/src/App.jsx
+++ 03_cards_proj1/src/App.jsx
@@ -2,65 +2,78 @@ import React from "react";
 import Card from "./components/card";
 import Navbar from "./components/navbar";
 const App = () => {
+  const jobs = [
+  {
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlLbZlt3X2TpFksku9MgE-cs0oxTzqAH36CQ&s",
+    jobname: "Apple",
+    days: "8 days ago",
+    jobTitle: "Senior Graphic Designer",
+    location: "NYC, USA"
+  },
+  {
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZI78WvDPQ241thhVYKtVphlc_J01LbnFVqA&s",
+    jobname: "Google",
+    days: "2 days ago",
+    jobTitle: "UI/UX Designer",
+    location: "California, USA"
+  },
+  {
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zdLpQ1XogOHnpR9MuW1MD9oXYxuPS5FCgQ&s",
+    jobname: "Microsoft",
+    days: "5 days ago",
+    jobTitle: "Frontend Developer",
+    location: "Seattle, USA"
+  },
+  {
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGGLk0CbrAGDbYcrxy5g2mDeoc4_FM6tNc5w&s",
+    jobname: "Amazon",
+    days: "1 day ago",
+    jobTitle: "Product Designer",
+    location: "Texas, USA"
+  },
+  {
+    image : "https://i.pinimg.com/736x/70/80/b2/7080b24c3980c8f4d29412746b5c7ce9.jpg",
+    jobname: "Meta",
+    days: "3 days ago",
+    jobTitle: "Visual Designer",
+    location: "Menlo Park, USA"
+  },
+  {
+    image : "https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-history-apple-inc-wikipedia-22.png",
+    jobname: "Netflix",
+    days: "6 days ago",
+    jobTitle: "Creative Designer",
+    location: "Los Angeles, USA"
+  },
+  {
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3AjLz2Ze01BXCesLCvOhwrlz9qHboz6XEwQ&s",
+    jobname: "Tesla",
+    days: "4 days ago",
+    jobTitle: "Brand Designer",
+    location: "Austin, USA"
+  },
+  {
+    image : "https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-history-apple-inc-wikipedia-22.png",
+    jobname: "Adobe",
+    days: "7 days ago",
+    jobTitle: "Motion Graphic Designer",
+    location: "San Jose, USA"
+  }
+];
+
+
   return (
     <div className="flex flex-wrap">
       <Navbar />
-      <Card
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlLbZlt3X2TpFksku9MgE-cs0oxTzqAH36CQ&s"
-        jobname="Amazon"
-        days="5 days ago"
-        jobTitle="Senior UI/UX designer"
-        location="Texas, USA"
-      />
-      <Card
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZI78WvDPQ241thhVYKtVphlc_J01LbnFVqA&s"
-        jobname="Google"
-        days="2 days ago"
-        jobTitle="Graphic designer"
-        location="Florida, USA"
-      />
-      <Card
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zdLpQ1XogOHnpR9MuW1MD9oXYxuPS5FCgQ&s"
-        jobname="Dribble"
-        days="2 days ago"
-        jobTitle="Senior motion designer"
-        location="London, UK"
-      />
-      <Card
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGGLk0CbrAGDbYcrxy5g2mDeoc4_FM6tNc5w&s"
-        jobname="Figma"
-        days="5 days ago"
-        jobTitle="UX designer"
-        location="Pakistan, Karachi"
-      />
-      <Card
-        image="https://i.pinimg.com/736x/70/80/b2/7080b24c3980c8f4d29412746b5c7ce9.jpg"
-        jobname="Airbnb"
-        days="6 days ago"
-        jobTitle="Junior UX designer"
-        location="Florida, USA"
-      />
-      <Card
-        image="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-history-apple-inc-wikipedia-22.png"
-        jobname="Apple"
-        days="8 days ago"
-        jobTitle="Senior graphic designer"
-        location="Florida, USA"
-      />
-      <Card
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3AjLz2Ze01BXCesLCvOhwrlz9qHboz6XEwQ&s"
-        jobname="Microsoft"
-        days="9 days ago"
-        jobTitle="Data Analyst"
-        location="Japan, Tokyo"
-      />
-      <Card
-        image="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-history-apple-inc-wikipedia-22.png"
-        jobname="Apple"
-        days="8 days ago"
-        jobTitle="Senior graphic designer"
-        location="NYC, USA"
-      />
+       {jobs.map((elem, index)=>{
+        return <Card 
+        key = {index}
+        image = {elem.image}
+        jobname = {elem.jobname}
+        days = {elem.days}
+        jobTitle = {elem.jobTitle}
+        location = {elem.location}/>
+       })}
     </div>
   );
 };
