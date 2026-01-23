d6b6f71405f08eca4f8a3c8d730669dc59d2d2fa
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-23 11:47:28 +0500

    inner and outer functioin practiced

--- 05_functions/src/App.jsx
+++ 05_functions/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+  const fruit = ()=>{
+    console.log("bahar wala");
+  }
+
 const App = () => {
   const clicked = (elem) =>{
     alert("Button clicked:")
@@ -8,13 +12,13 @@ const App = () => {
 
   // if we want to pass parameters in the function:
   
-  const fruit = (elem)=>{
-    console.log(elem);
+  const fruit = ()=>{
+    console.log("andar wala");
   }
   return (
     <div className='h-screen w-full bg-zinc-900 '> 
       <button className='bg-gray-700 m-2 rounded text-white p-1 font-bold' onClick={clicked}>Click ME</button>
-      <button className='bg-gray-700 m-2 rounded text-white p-1 font-bold' onClick={()=> fruit("apple")}>Click for Fruits:</button>
+      <button className='bg-gray-700 m-2 rounded text-white p-1 font-bold' onClick={fruit}>Click for Fruits:</button>
     </div>
   )
 }
