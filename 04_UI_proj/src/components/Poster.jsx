b3b92fb8125f036102b9ed67e8a356e2cd2ce414
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-23 18:21:41 +0500

    scrolling cards all ready

---
+++ 04_UI_proj/src/components/Poster.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Poster = () => {
+  return (
+    <div>
+      <div className='h-130 w-70 bg-blue-400 m-10 rounded-[20px]'></div>
+    </div>
+  )
+}
+
+export default Poster
--- 04_UI_proj/src/components/Secondcard.jsx
+++ 04_UI_proj/src/components/Secondcard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Poster from './Poster'
 const Secondcard = () => {
   return (
-    <div className='bg-orange-500 w-2/3'>Secondcard</div>
+    <div id='right' className='bg-gray-300 w-2/3 flex overflow-x-auto flex-nowrap rounded-[20px]'>
+    <Poster />
+    <Poster />
+    <Poster />
+    <Poster />
+    <Poster />
+    </div>
   )
 }
 
