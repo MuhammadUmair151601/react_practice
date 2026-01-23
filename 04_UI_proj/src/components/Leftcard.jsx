619a5de963527b4a927e451b6a0dc6707d8cc067
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-23 17:34:52 +0500

    left card done

--- 04_UI_proj/src/components/Leftcard.jsx
+++ 04_UI_proj/src/components/Leftcard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { ArrowUpRight } from "lucide-react"
 
 const Leftcard = () => {
   return (
-    <div className='w-1/3 bg-black h-150'>Leftcard</div>
+    <div className='w-1/3 h-150 text-black'>
+      <p className='text-6xl font-bold '>prospective customer segmentation</p>
+      <p className='text-xl mt-20 text-gray-600 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident porro ad ipsa non quas incidunt obcaecati sit in vitae aperiam?</p>
+      <div className='mt-45'><ArrowUpRight size={75}/></div>
+       
+    </div>
   )
 }
 
