de844ca051fd897b474e2ab26b8bfc7ca42ef447
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-26 18:31:28 +0500

    layout ready

--- 04_UI_proj/src/components/Poster.jsx
+++ 04_UI_proj/src/components/Poster.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const Poster = (props) => {
-  // const image = props;
   return (
     <div>
-      <div className='poster h-130 w-70 bg-blue-400 m-10 rounded-[20px] p-5 flex flex-col justify-between'>
-        {/* style={{ backgroundImage: `url(${props.image})` }} */}
+      <div className='h-150 w-75 poster bg-cover bg-center bg-no-repeat  m-10 rounded-[20px] p-5 flex flex-col justify-between opacity-90'
+      style={{ backgroundImage: `url(${props.image})` }}
+      >
         <h1 className='h-12 w-12 bg-white rounded-[50px] flex justify-center items-center font-bold text-2xl '>{props.id}</h1>
-        <p className='text-xl mt-25'>{props.detail}</p>
-        <button className='bg-blue-500 h-10 w-35 mt-5 rounded-[25px] mb-10'>{props.button}</button>
+        <p className='text-xl mt-25 text-white'>{props.detail}</p>
+        <button style={{backgroundColor:props.color}} className='bg-blue-600 h-10 w-35 mt-5 rounded-[25px] mb-10 text-xl font-bold text-black transition-transform 
+  duration-300 
+  ease-in-out 
+  hover:scale-x-[1.1]  cursor-pointer'>{props.button}</button>
       </div>
     </div>
   )
--- 04_UI_proj/src/components/Secondcard.jsx
+++ 04_UI_proj/src/components/Secondcard.jsx
@@ -2,48 +2,54 @@ import React from "react";
 import Poster from "./Poster";
 
 const Secondcard = () => {
-  const data = [
-    {
-      id: 1,
-      image:
-        "https://images.unsplash.com/photo-1768036479323-a30050ace29f?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      detail:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis porro iusto delectus doloremque dolor? Animi blanditiis labore ullam illo minus?",
-      button: "hire me",
-    },
-    {
-      id: 2,
-      image:
-        "https://images.unsplash.com/photo-1601845342792-636d6231f8f7?q=80&w=648&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      detail:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, voluptate. Vel magnam quaerat ex incidunt doloremque possimus dolores enim iste!",
-      button: "get details",
-    },
-    {
-      id: 3,
-      image:
-        "https://images.unsplash.com/photo-1624359209425-05eb9b809795?q=80&w=536&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      detail:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere cum, sequi cupiditate quisquam incidunt nesciunt voluptatem magnam quasi ipsum pariatur.",
-      button: "Contact us",
-    },
-    {
-      id: 4,
-      image:
-        "https://images.unsplash.com/photo-1579547944082-fac44e416258?q=80&w=639&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      detail:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptates ipsam dolorem blanditiis itaque autem accusamus nisi pariatur officiis ullam?",
-      button: "feedback",
-    },
-    {
-      id: 5,
-      image:
-        "https://images.unsplash.com/photo-1615714464608-334b4805748c?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      detail:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel temporibus id dolor ipsa eum debitis harum itaque molestias excepturi quae.",
-      button: "Let us know",
-    },
-  ];
+ const data = [
+  {
+    id: 1,
+    color: "orange",
+    image:
+      "https://plus.unsplash.com/premium_photo-1675553742348-dbb89004b2a2?q=80&w=682&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis porro iusto delectus doloremque dolor? Animi blanditiis labore ullam illo minus?",
+    button: "hire me",
+  },
+  {
+    id: 2,
+    color: "pink",
+    image:
+      "https://images.unsplash.com/photo-1601845342792-636d6231f8f7?q=80&w=648&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, voluptate. Vel magnam quaerat ex incidunt doloremque possimus dolores enim iste!",
+    button: "get details",
+  },
+  {
+    id: 3,
+    color: "blue",
+    image:
+      "https://images.unsplash.com/photo-1624359209425-05eb9b809795?q=80&w=536&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere cum, sequi cupiditate quisquam incidunt nesciunt voluptatem magnam quasi ipsum pariatur.",
+    button: "Contact us",
+  },
+  {
+    id: 4,
+    color: "green",
+    image:
+      "https://images.unsplash.com/photo-1579547944082-fac44e416258?q=80&w=639&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptates ipsam dolorem blanditiis itaque autem accusamus nisi pariatur officiis ullam?",
+    button: "feedback",
+  },
+  {
+    id: 5,
+    color: "yellow",
+    image:
+      "https://images.unsplash.com/photo-1615714464608-334b4805748c?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel temporibus id dolor ipsa eum debitis harum itaque molestias excepturi quae.",
+    button: "Let us know",
+  },
+];
+
   return (
     <div
       id="right"
@@ -52,10 +58,11 @@ const Secondcard = () => {
       {data.map((elem, idx) => (
         <Poster
           key={idx}
-          //  image = {elem.image}
+           image = {elem.image}
           id={elem.id}
           detail={elem.detail}
           button = {elem.button}
+          color = {elem.color}
         />
       ))}
     </div>
