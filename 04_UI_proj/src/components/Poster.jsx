b1d2b2d6fe78321a3eaf443253aecfc55d03ee71
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-26 17:43:51 +0500

    posters props passed except images

--- 04_UI_proj/src/components/Poster.jsx
+++ 04_UI_proj/src/components/Poster.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Poster = (props) => {
-
+  // const image = props;
   return (
     <div>
-      <div className='h-130 w-70 bg-blue-400 m-10 rounded-[20px]'></div>
+      <div className='poster h-130 w-70 bg-blue-400 m-10 rounded-[20px] p-5 flex flex-col justify-between'>
+        {/* style={{ backgroundImage: `url(${props.image})` }} */}
+        <h1 className='h-12 w-12 bg-white rounded-[50px] flex justify-center items-center font-bold text-2xl '>{props.id}</h1>
+        <p className='text-xl mt-25'>{props.detail}</p>
+        <button className='bg-blue-500 h-10 w-35 mt-5 rounded-[25px] mb-10'>{props.button}</button>
+      </div>
     </div>
   )
 }
