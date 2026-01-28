1aff00ecc0843a5ce35dcd49eeb0213d2b81b693
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-28 17:40:38 +0500

    counter practiced using usestate

--- 06_usestate/src/App.jsx
+++ 06_usestate/src/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [Num, setNum] = useState(0)
+
+  const increase = () =>{
+    setNum(Num + 1)
+  }
+
+  const decrease = () =>{
+    setNum(Num - 1)
+  }
+
+  const jump5 = () =>{
+    setNum(Num + 5)
+  }
+
+  const zero = () =>{
+    setNum(0)
+  }
   return (
-    <div className=''>App</div>
+    <div className=''>
+      <h1 className=''>count: {Num}</h1>
+      <button onClick={increase}>increment</button>
+      <button onClick={decrease}>decrement</button>
+      <button onClick={jump5}>Jump 5 values</button>
+      <button onClick={zero}>clear</button>
+    </div>
   )
 }
 
