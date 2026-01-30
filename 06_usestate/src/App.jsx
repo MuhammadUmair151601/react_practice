47d1575cf4ed1663a9866dd5b94ba71a89a5fb2e
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-30 12:52:31 +0500

    usestates done

--- 06_usestate/src/App.jsx
+++ 06_usestate/src/App.jsx
@@ -1,32 +1,54 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-const App = () => {
-  const [Num, setNum] = useState(0)
+// const App = () => {
+//   const [Num, setNum] = useState(0)
 
-  const increase = () =>{
-    setNum(Num + 1)
-  }
+//   const increase = () =>{
+//     setNum(Num + 1)
+//   }
 
-  const decrease = () =>{
-    setNum(Num - 1)
-  }
+//   const decrease = () =>{
+//     setNum(Num - 1)
+//   }
 
-  const jump5 = () =>{
-    setNum(Num + 5)
-  }
+//   const jump5 = () =>{
+//     setNum(Num + 5)
+//   }
 
-  const zero = () =>{
-    setNum(0)
-  }
-  return (
-    <div className=''>
-      <h1 className=''>count: {Num}</h1>
-      <button onClick={increase}>increment</button>
-      <button onClick={decrease}>decrement</button>
-      <button onClick={jump5}>Jump 5 values</button>
-      <button onClick={zero}>clear</button>
-    </div>
-  )
-}
+//   const zero = () =>{
+//     setNum(0)
+//   }
+//   return (
+//     <div className=''>
+//       <h1 className=''>count: {Num}</h1>
+//       <button onClick={increase}>increment</button>
+//       <button onClick={decrease}>decrement</button>
+//       <button onClick={jump5}>Jump 5 values</button>
+//       <button onClick={zero}>clear</button>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
+
+// import React from 'react'
+// import { useState } from 'react'
+// const App = () => {
+//   const [Num, setNum] = useState([1,2,3,4,5])
+
+//   const btnclicked = () =>{
+//     const newNum = [...Num]
+//     setNum(newNum)
+//     newNum.push(99)
+    
+//     console.log(newNum)
+//   }
+//   return (
+//     <div>
+//       <button onClick={btnclicked}>Click Here</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
