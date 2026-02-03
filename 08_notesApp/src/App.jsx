36aae69ef73d06a97ead876176d22c0d5f7651eb
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-02-03 16:42:48 +0500

    notes portion to right side done

--- 08_notesApp/src/App.jsx
+++ 08_notesApp/src/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const submithandler = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted:");
+  };
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex justify-betweenm px-20 items-center h-full bg-black ">
+      
+      <div className="min-h-screen lg:flex lg:h-full">
+        
+        <form
+          onSubmit={(e) => {
+            submithandler(e);
+          }}
+          className="flex flex-col gap-5 px-20 pt-5 w-150"
+        >
+          <h1 className="text-3xl text-white font-bold">Add Notes</h1>
+          <input
+            className="bg-gray-700 h-15 px-10 outline-none text-xl text-white border-2 rounded"
+            type="text"
+            placeholder="Enter notes heading"
+          />
+          <textarea
+            className="bg-gray-700 h-35 outline-none py-2 px-10 text-xl text-white  border-2 rounded"
+            type="text"
+            placeholder="Enter Details"
+          />
+          <button className="p-5 text-black bg-white border-2 rounded text-2xl font-bold cursor-pointer">
+            Add Notes
+          </button>
+        </form>
+        <div className="mt-5 lg:w-1/2 p-10 flex flex-col">
+          <h1 className="text-3xl font-bold text-white">My Notes</h1>
+          <div className="flex mt-5 gap-5 flex-wrap h-full overflow-y-auto">
+            <div className="h-50 w-50 p-5 bg-white mt-10 rounded-[5px]"></div>
+            <div className="h-50 w-50 p-5 bg-white mt-10 rounded-[5px]"></div>
+            <div className="h-50 w-50 p-5 bg-white mt-10 rounded-[5px]"></div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
