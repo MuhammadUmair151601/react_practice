8ce5e7e30d3efbda69173b7e17dada1fbb6d8293
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-02-14 13:57:56 +0500

    API use in UI done

--- 10_APIcalling/src/App.jsx
+++ 10_APIcalling/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
-   
+
 // const getdata = ()=>{
 //  const response = fetch('https://jsonplaceholder.typicode.com/todos/1')
 //  console.log(response)
@@ -23,16 +23,37 @@ const App = () => {
 // }
 
 // now calling apis using axios:
+// const getdata = async ()=>{
+//   const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+//   console.log(response.data)
+// }
+
+// now how to show the fetched data in UI:
+const [data, setdata] = useState([])
+
 const getdata = async ()=>{
-  const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
-  console.log(response.data)
+  const response = await axios.get('https://dummyjson.com/users')
+  setdata(response.data.users)
+  // console.log(response.data)
 }
 
 
   return (
     <div>
-      <button onClick={getdata}>Get data</button>
-    </div>
+      <button onClick={getdata}>get data</button>
+      
+      <div>
+      {data.map((elem,idx)=>{
+        return (
+          <div>
+        <h1>Hello, {elem.firstName} {elem.lastName}</h1>
+        <img src={elem.image} width="100" />
+        <p>{elem.address.city}</p>
+        </div>
+        )
+      })}
+      </div>
+  </div>
   )
 }
 
